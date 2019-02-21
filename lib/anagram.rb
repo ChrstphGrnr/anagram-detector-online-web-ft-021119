670fb0b0f7632d14e0a0bd3fed1d21fc8d9e1368
Christoph Gruener <christophgruener@me.com>
2019-02-21 22:52:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 class Anagram 
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(annagrams)
+    match_arr = []
+    annagrams.split.each do |words|
+      if words.chars.sort_by(&:downcase).join == @word.chars.sort_by(&:downcase)
+        match_arr << words
+      end
+      match_arr
+    end
+end
